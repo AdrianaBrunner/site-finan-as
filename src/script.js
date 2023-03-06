b4e6f79084009d97cff255c97fdc005f1fe8e4b3
Adriana Brunner <a.drk63@gmail.com>
2023-03-06 13:24:22 -0300

    ajuste cards

--- src/script.js
+++ src/script.js
@@ -1,17 +1,47 @@
 
 const form = document.querySelector("form");
 const lista = document.getElementById("lista")
-//const card = document.getElementsByClassName("card");
+
+let totalReceitas = 0;
+let totalDespesas = 0;
+let total = 0;
+
+let receitas = document.querySelector("#receitas")
+let despesas = document.querySelector("#despesas")
+let valorTotal = document.querySelector("#total")
+
+let pReceitas = document.querySelector("#pReceitas")
+let pDespesas = document.querySelector("#pDespesas")
+let pTotal = document.querySelector("#pTotal")
 
 let transacoes = JSON.parse(localStorage.getItem("transacoes")) || [];
 
 function adicionar(event) {
     event.preventDefault();
-    const valor = parseFloat(document.getElementById("valor").value);
-    var valorFormatado = valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    let valor = parseFloat(document.getElementById("valor").value);
+    let valor1 = Number(valor);
+    var valorFormatado = valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
     const selected = document.getElementById("selected").value;
-    const transacao = {valorFormatado, selected};
+    const transacao = { valor1, valorFormatado, selected };
     transacoes.push(transacao);
+
+
+    if (transacao.selected == "Receita") {
+        totalReceitas += transacao.valor1
+    } else if (transacao.selected == "Despesa") {
+        totalDespesas -= transacao.valor1
+    }
+
+    total = totalReceitas + totalDespesas
+
+    pReceitas.innerHTML = `R$ ${totalReceitas}`;
+    pDespesas.innerHTML = `R$ ${totalDespesas}`
+    pTotal.innerHTML = `R$ ${total}`
+
+    valorTotal.appendChild(pTotal);
+    despesas.appendChild(pDespesas);
+    receitas.appendChild(pReceitas)
+
     atualizar();
     mostrar();
     form.reset();
@@ -19,24 +49,36 @@ function adicionar(event) {
 
 function mostrar() {
     lista.innerHTML = "";
+
     transacoes.forEach((transacao, i) => {
         const itemLista = document.createElement("li");
-        itemLista.innerHTML = `${transacao.valorFormatado} - ${transacao.selected}  <button class="deletar" data-i="${i}">
+        itemLista.innerHTML = `${transacao.valorFormatado} - ${transacao.selected}  
+        <button class="deletar" data-i="${i}">
         Excluir</button>`;
         lista.appendChild(itemLista);
-        if(transacao.selected == "Receita") {
+
+        if (transacao.selected == "Receita") {
             itemLista.style.color = "green";
-            let valorReceita = transacao.valorFormatado;
         } else {
             itemLista.style.color = "red";
-            let valorDespesa = transacao.valorFormatado;
         }
     })
+
 }
 
 function deletar(event) {
-    if(event.target.classList.contains("deletar")) {
+    if (event.target.classList.contains("deletar")) {
         const i = event.target.getAttribute("data-i");
+        if (transacoes[i].selected == "Receita") {
+            totalReceitas -= transacoes[i].valor1
+            pReceitas.innerHTML = `R$ ${totalReceitas}`;
+        }
+        if (transacoes[i].selected == "Despesa") {
+            totalDespesas += transacoes[i].valor1
+            pDespesas.innerHTML = `R$ ${totalDespesas}`;
+        };
+        total = totalReceitas + totalDespesas
+        pTotal.innerHTML = `R$ ${total}`
         transacoes.splice(i, 1);
         atualizar();
         mostrar();
